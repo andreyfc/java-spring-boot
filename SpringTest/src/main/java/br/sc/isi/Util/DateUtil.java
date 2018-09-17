2e96f5c6ed198949d85ab973750b527e6cdd7a04
Andrey freitas cassio <andrey.cassio@sc.senai.br>
2018-09-17 11:02:45 -0300

    Second

---
+++ SpringTest/src/main/java/br/sc/isi/Util/DateUtil.java
@@ -0,0 +1,11 @@
+package br.sc.isi.Util;
+
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
+public class DateUtil {
+    public String formatLocalDateTimeToDatabaseStyle(LocalDateTime localDateTime){
+        return DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss").format(localDateTime);
+    }
+
+}
--- SpringTest/src/main/java/br/sc/isi/endpoint/StudentEndPoint.java
+++ SpringTest/src/main/java/br/sc/isi/endpoint/StudentEndPoint.java
@@ -1,8 +1,11 @@
 package br.sc.isi.endpoint;
 
+import br.sc.isi.Util.DateUtil;
 import br.sc.isi.model.Student;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
+import java.time.LocalDateTime;
 import java.util.List;
 
 import static java.util.Arrays.asList;
@@ -10,10 +13,11 @@ import static java.util.Arrays.asList;
 @RestController
 @RequestMapping("student")
 public class StudentEndPoint {
+    @Autowired
+    private DateUtil dateUtil;
     @RequestMapping(method = RequestMethod.GET, path = "/list")
     public List<Student> listAll() {
+        System.out.println(dateUtil.formatLocalDateTimeToDatabaseStyle(LocalDateTime.now()));
         return asList(new Student("Deku"),new Student("Todoroki"), new Student("Andrey"));
     }
 }
-
-git remote set-url origin https:https://github.com/andreyfc/java-spring-boot.git
