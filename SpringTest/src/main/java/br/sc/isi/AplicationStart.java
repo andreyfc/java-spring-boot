b3c82721063bd1584f12ff1b2442f8dc30062790
Andrey freitas cassio <andrey.cassio@sc.senai.br>
2018-09-17 11:53:55 -0300

    Second

--- SpringTest/src/main/java/br/sc/isi/AplicationStart.java
+++ SpringTest/src/main/java/br/sc/isi/AplicationStart.java
@@ -1,11 +1,11 @@
-package br.sc.isi.start;
+package br.sc.isi;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
 
-@EnableAutoConfiguration
-@ComponentScan(basePackages = "br.sc.isi.endpoint")
+@SpringBootApplication // @Configuration @EnableAutoConfiguration @ComponentScan
 public class AplicationStart {
     public static void main(String[] args) {
         SpringApplication.run(AplicationStart.class, args);
--- SpringTest/src/main/java/br/sc/isi/Util/DateUtil.java
+++ SpringTest/src/main/java/br/sc/isi/Util/DateUtil.java
@@ -1,11 +1,13 @@
 package br.sc.isi.Util;
 
-import java.time.LocalDateTime;
-import java.time.format.DateTimeFormatter;
+        import org.springframework.stereotype.Component;
 
+        import java.time.LocalDateTime;
+        import java.time.format.DateTimeFormatter;
+
+@Component
 public class DateUtil {
-    public String formatLocalDateTimeToDatabaseStyle(LocalDateTime localDateTime){
-        return DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss").format(localDateTime);
+    public String formatLocalDateTimeToDatabaseStyle(LocalDateTime localDateTime) {
+        return DateTimeFormatter.ofPattern("-----"+"yyyy-MM-dd HH:mm:ss").format(localDateTime);
     }
-
 }
