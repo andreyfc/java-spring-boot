1d0dcfeed6e011befa411b26018336ca98229969
andreyfc <andrey.freitascassio@gmail.com>
2018-10-15 13:11:07 -0300

    JWTAutorization

--- springboot-initializer/src/main/java/br/andrey/spring/config/SecurityConfig.java
+++ springboot-initializer/src/main/java/br/andrey/spring/config/SecurityConfig.java
@@ -2,27 +2,49 @@ package br.andrey.spring.config;
 
 import br.andrey.spring.service.CustomUserDetailService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import br.andrey.spring.service.CustomUserDetailService;
+
+import static br.andrey.spring.config.SecurityConstants.SIGN_UP_URL;
 
 @EnableWebSecurity
 @EnableGlobalMethodSecurity(prePostEnabled = true)
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
     @Autowired
     private CustomUserDetailService customUserDetailService;
+//    @Override
+//    protected void configure(HttpSecurity http) throws Exception {
+//        http.authorizeRequests()
+//                .antMatchers("/*/protected/**").hasRole("USER")
+//                .antMatchers("/*/admin/students/**").hasRole("ADMIN")
+//                .and()
+//                .httpBasic()
+//        .and()
+//        .csrf().disable();
+//    }
+
     @Override
     protected void configure(HttpSecurity http) throws Exception {
+        http.cors().and().csrf().disable().authorizeRequests()
+                .antMatchers(HttpMethod.GET, SIGN_UP_URL).permitAll()
+                .antMatchers("/*/protected/**").hasRole("USER")
+                .antMatchers("/*/admin/students/**").hasRole("ADMIN")
+                .and()
+                .addFilter(new JWTAthenticationFilter(authenticationManager()))
+                .addFilter(new JWTAuthorizationFilter(authenticationManager(), customUserDetailService));
         http.authorizeRequests()
                 .antMatchers("/*/protected/**").hasRole("USER")
                 .antMatchers("/*/admin/students/**").hasRole("ADMIN")
                 .and()
                 .httpBasic()
-        .and()
-        .csrf().disable();
+                .and()
+                .csrf().disable();
     }
 
     @Override
